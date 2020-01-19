4a192c092444ff48933b26afdbc8070358f3731a
Summar Raja <summarkimail@gmail.com>
2020-01-19 17:07:07 +0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+mod Punjab{
+    pub mod Lahore{
+        pub fn model_town(){
+            println!("VERY BAD");
+        }
+    }
+}
+mod lib;
+fn main() {
+    Punjab::Lahore::model_town();
+    lib::kpk::peshawar();
+}
